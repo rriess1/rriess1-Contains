b3638f627f840d69b11600959f586fea629c7aa9
rriess1 <robertwriess@yahoo.com>
2023-03-02 17:16:53 +0000

    first

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,12 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        for(int i = 0; i < arr.length;i++){
+
+            if(target == arr[i]){
+                return true;
+            }
+        }
         return false;
     }
 }
